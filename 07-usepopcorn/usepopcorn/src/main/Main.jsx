36128208419c1c53362b4d6c4e7163e393220dce
Lutogin <aluto@softserveinc.com>
2023-12-07 20:52:18 +0100

    use-popcorn composition

--- 07-usepopcorn/usepopcorn/src/main/Main.jsx
+++ 07-usepopcorn/usepopcorn/src/main/Main.jsx
@@ -1,6 +1,7 @@
 import { ListBox } from './ListBox/ListBox';
 import { WatchedBox } from './WatchedBox/WatchedBox';
 import { useState } from 'react';
+import { NavBar } from './NavBar/NavBar';
 
 const tempWatchedData = [
   {
@@ -25,16 +26,46 @@ const tempWatchedData = [
   },
 ];
 
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
 
-export function Main({ movies }) {
+
+
+export function Main() {
+  const [movies, setMovies] = useState(tempMovieData);
 
   const [watched, setWatched] = useState(tempWatchedData);
 
   return (
-    <main className="main">
-      <ListBox movies={movies}/>
+    <>
+      <NavBar movies={movies} setMovies={setMovies} />
+      <main className="main">
+        <ListBox movies={movies}/>
 
-      <WatchedBox watched={watched}/>
-    </main>
+        <WatchedBox watched={watched}/>
+      </main>
+    </>
+
   )
 }
